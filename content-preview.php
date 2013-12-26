01ef3b1003d1c4ef2f0c93118f355a1d682f4b24
William M Riley <bill_riley@Williams-MacBook-Pro.local>
2013-12-26 11:45:20 -0600

    excerpt bug

--- content-preview.php
+++ content-preview.php
@@ -2,7 +2,7 @@
 	<a href="<?php the_permalink(); ?>" class="ajax">
 		<img src="<?php echo (get_post_thumbnail_id( $post->ID )) ? wp_get_attachment_url( get_post_thumbnail_id( $post->ID ) ) : print_backup_image_url() ; ?>" />
 		<header><?php the_title(); ?></header>
-		<section><?php the_excerpt(); ?></section>
+		<section><?php the_content(); ?></section>
 		<footer><?php the_date(); ?></footer>
 	</a>
 </article>
