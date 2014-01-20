7513171e73877969e990e693b83fe7f1257ea382
William M Riley <bill_riley@Williams-MacBook-Pro.local>
2014-01-19 23:37:18 -0600

    Social updates

--- single-social.php
+++ single-social.php
@@ -1,8 +1,8 @@
 <div class="entry-social">
-	<a class="btn btn-tweet" target="_blank" href="https://twitter.com/intent/tweet?text=YOUR-TITLE&url=YOUR-URL&via=TWITTER-HANDLE"><b class="mtk-twitter"></b></a>
-	<a class="btn btn-facebook" target="_blank" href="http://www.facebook.com/sharer/sharer.php?u=YOUR-URL"><b class="mtk-facebook"></b></a>
-	<a class="btn btn-google" target="_blank" href="https://plus.google.com/share?url=YOUR-URL"><b class="mtk-google-plus"></b></a>
-	<a class="btn btn-linkedin" target="_blank" href="http://www.linkedin.com/shareArticle?mini=true&url=YOUR-URL&title=YOUR-TITLE&summary=YOUR-SUMMARY&source=YOUR-URL"><b class="mtk-linkedin"></b></a>
-	<a class="btn btn-pinterest" target="_blank" href="http://pinterest.com/pin/create/button/?url=YOUR-URL&description=YOUR-DESCRIPTION&media=YOUR-IMAGE-SRC"><b class="mtk-pinterest"></b></a>
-	<?php if (true): ?><a class="btn btn-github" target="_blank" href="#"><b class="mtk-github"></b></a><?php endif; ?>
+	<a class="btn btn-tweet" target="_blank" href="https://twitter.com/intent/tweet?text=<?php echo the_title(); ?>&url=<?php echo the_permalink(); ?>&via=@bill_riley"><b class="mtk-twitter"></b></a>
+	<a class="btn btn-facebook" target="_blank" href="http://www.facebook.com/sharer/sharer.php?u=<?php echo the_permalink(); ?>"><b class="mtk-facebook"></b></a>
+	<a class="btn btn-google" target="_blank" href="https://plus.google.com/share?url=<?php echo the_permalink(); ?>"><b class="mtk-google-plus"></b></a>
+	<a class="btn btn-linkedin" target="_blank" href="http://www.linkedin.com/shareArticle?mini=true&url=<?php echo the_permalink(); ?>&title=<?php echo the_title(); ?>&summary=<?php echo the_subtitle(); ?>&source=<?php echo the_permalink(); ?>"><b class="mtk-linkedin"></b></a>
+	<a class="btn btn-pinterest" target="_blank" href="http://pinterest.com/pin/create/button/?url=<?php echo the_permalink(); ?>&description=<?php echo the_title(); ?>&media=<?php echo (get_post_thumbnail_id( $post->ID )) ? wp_get_attachment_url( get_post_thumbnail_id( $post->ID ) ) : print_backup_image_url() ; ?>"><b class="mtk-pinterest"></b></a>
+	<?php if (true == false): ?><a class="btn btn-github" target="_blank" href="#"><b class="mtk-github"></b></a><?php endif; ?>
 </div>
