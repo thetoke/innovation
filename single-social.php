5ba7bec9353638bb5e06a5b9b786d84ab76d6456
William M Riley <bill_riley@Williams-MacBook-Pro.local>
2014-01-19 23:38:27 -0600

    twitter fix

--- single-social.php
+++ single-social.php
@@ -1,5 +1,5 @@
 <div class="entry-social">
-	<a class="btn btn-tweet" target="_blank" href="https://twitter.com/intent/tweet?text=<?php echo the_title(); ?>&url=<?php echo the_permalink(); ?>&via=@bill_riley"><b class="mtk-twitter"></b></a>
+	<a class="btn btn-tweet" target="_blank" href="https://twitter.com/intent/tweet?text=<?php echo the_title(); ?>&url=<?php echo the_permalink(); ?>&via=bill_riley"><b class="mtk-twitter"></b></a>
 	<a class="btn btn-facebook" target="_blank" href="http://www.facebook.com/sharer/sharer.php?u=<?php echo the_permalink(); ?>"><b class="mtk-facebook"></b></a>
 	<a class="btn btn-google" target="_blank" href="https://plus.google.com/share?url=<?php echo the_permalink(); ?>"><b class="mtk-google-plus"></b></a>
 	<a class="btn btn-linkedin" target="_blank" href="http://www.linkedin.com/shareArticle?mini=true&url=<?php echo the_permalink(); ?>&title=<?php echo the_title(); ?>&summary=<?php echo the_subtitle(); ?>&source=<?php echo the_permalink(); ?>"><b class="mtk-linkedin"></b></a>
